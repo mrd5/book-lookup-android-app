26594c29dd5f7a147fc3f1b77b9d1ea26a63a46f
Mathew Dorish <mrd@email.ca>
2018-05-21 16:58:13 -0600

    Now displays both book title and description (or Book not found)

--- app/src/main/java/com/example/mathew/bookscanner/connectConnection.java
+++ app/src/main/java/com/example/mathew/bookscanner/connectConnection.java
@@ -2,12 +2,24 @@ package com.example.mathew.bookscanner;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLConnection;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 /**
  * Created by Mathew on 18-May-18.
@@ -15,27 +27,71 @@ import java.net.URLConnection;
 
 public class connectConnection extends AppCompatActivity {
     String isbn;
+    String url;
+    TextView bookTitle;
+    TextView bookDesc;
+    private RequestQueue mRequestQueue;
+    private StringRequest stringRequest;
+    private static final String TAG = connectConnection.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lookup_barcode);
 
         isbn = getIntent().getStringExtra("EXTRA_BARCODE_VALUE");
-
+        bookTitle = (TextView) findViewById(R.id.bookTitle);
+        bookDesc = (TextView) findViewById(R.id.bookDesc);
+        url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn;
 
         getResults();
     }
 
     private void getResults(){
-        try{
-            URL url = new URL("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn);
-            URLConnection request = url.openConnection();
-            request.connect();
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i(TAG, response.toString());
+                String jsonData = response;
+
+                try {
+                    JSONObject main = new JSONObject(jsonData);
+
+                    if (main.getInt("totalItems") == 0){
+                        bookTitle.setText("Book not found!");
+                        bookDesc.setText("");
+                    }
+                    else{
+                        JSONArray items = main.getJSONArray("items");
+                        JSONObject book = items.getJSONObject(0);
+
+                        JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                        String bookName = volumeInfo.getString("title");
+                        String bookDescription = volumeInfo.getString("description");
 
 
-        } catch (IOException e){
-            e.printStackTrace();
-        }
+                        bookTitle.setText(bookName);
+                        bookDesc.setText(bookDescription);
+                    }
+
+                } catch (Exception ex){
+                    ex.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                Log.i(TAG, error.toString());
+            }
+        });
+
+        mRequestQueue.add(stringRequest);
+
+
 
     }
 }
