a9e711aa59496ac957f5649d13705817c187d1db
Mathew Dorish <mathewdorish@hotmail.com>
2018-05-30 20:18:34 -0600

    Just created

--- app/src/main/java/com/example/mathew/bookscanner/goodreadsAuthorSearch.java
+++ app/src/main/java/com/example/mathew/bookscanner/goodreadsAuthorSearch.java
@@ -1,8 +1,19 @@
 package com.example.mathew.bookscanner;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by Mathew on 30-May-18.
  */
 
-public class goodreadsAuthorSearch {
+public class goodreadsAuthorSearch extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goodreads_author);
+
+    }
 }
